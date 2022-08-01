99c44eed5645ed3c25914778c5b75048875a6961
hyujisf <canqalloid.me@gmail.com>
2022-08-01 09:47:50 +0700

    Learn About Variables

---
+++ var.js
@@ -0,0 +1,8 @@
+var iceCream = "chocolate"
+console.log(iceCream)
+
+/* About Var
+Var adalah tipe data Javascript yang bersifat dan mencangkup Global
+sehingga dapat diubah hingga digunakan kembali,
+dan hal tersebut kurang baik, namun berguna dalam beberapa kondisi
+ */
